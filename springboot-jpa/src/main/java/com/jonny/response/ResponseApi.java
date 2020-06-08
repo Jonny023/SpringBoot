aadf89d3d495fa7485adb2f15ac605710bc78334
Jonny <30571119+Jonny023@users.noreply.github.com>
2020-06-08 18:34:54 +0800

    Update ResponseApi.java

--- springboot-jpa/src/main/java/com/jonny/response/ResponseApi.java
+++ springboot-jpa/src/main/java/com/jonny/response/ResponseApi.java
@@ -1,42 +1,53 @@
 package com.jonny.response;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
+/**
+* lombok泛型实例不能通过new创建，需要通过Class.builder()构建
+*/
 @Data
-@AllArgsConstructor
+@Builder
 public class ResponseApi<T> {
 
     private int code;
     private String msg;
     private T data;
 
+//    @Tolerate
+//    public ResponseApi(int code, String msg, T data) {
+//        this.code = code;
+//        this.msg = msg;
+//        this.data = data;
+//    }
+
     public static<T> ResponseApi<T> success(T data) {
-        return new ResponseApi(200, "请求成功", data);
+        return ResponseApi.<T>builder().code(200).msg("请求成功").data(data).build();
     }
 
     public static<T> ResponseApi<T> success(String msg) {
-        return new ResponseApi(200, msg, null);
+        return ResponseApi.<T>builder().code(200).msg(msg).data(null).build();
     }
 
     public static<T> ResponseApi<T> success(int code, String msg, T data) {
-        return new ResponseApi(code, msg, data);
+        return ResponseApi.<T>builder().code(code).msg(msg).data(data).build();
+
     }
 
     public static<T> ResponseApi<T> error(T data) {
-        return new ResponseApi(500, "异常", data);
+        return ResponseApi.<T>builder().code(500).msg("异常").data(data).build();
     }
 
     public static<T> ResponseApi<T> error() {
-        return new ResponseApi(500, "异常", null);
+        return ResponseApi.<T>builder().code(500).msg("异常").data(null).build();
     }
 
     public static<T> ResponseApi<T> error(String msg) {
-        return new ResponseApi(500, msg, null);
+        return ResponseApi.<T>builder().code(500).msg(msg).data(null).build();
     }
 
     public static<T> ResponseApi<T> error(int code, String msg, T data) {
-        return new ResponseApi(code, msg, data);
+        return ResponseApi.<T>builder().code(code).msg(msg).data(data).build();
     }
 
 }
