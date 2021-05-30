d550799f326dc44f68a080d4b263bb0750214fa8
Jonny <e-lijing6@geely.com>
2021-05-30 17:01:10 +0800

    junit

---
+++ spring-boot-unit/src/main/java/org/jonny/App.java
@@ -0,0 +1,18 @@
+package org.jonny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: e-lijing6
+ * @description:
+ * @date:created in 2021/5/30 15:36
+ * @modificed by:
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
