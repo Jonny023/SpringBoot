0a257f853e745ab69c7d1019cd542c9fae56ffbe
Jonny <e-lijing6@geely.com>
2021-07-08 18:42:14 +0800

    quartz

---
+++ spring-boot-quartz/src/main/java/com/example/springbootquartz/config/SchedulerConfig.java
@@ -0,0 +1,76 @@
+package com.example.springbootquartz.config;
+
+import org.quartz.Scheduler;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+@Configuration
+public class SchedulerConfig {
+
+    @Resource
+    private DataSource dataSource;
+
+    /**
+     * 调度器
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public Scheduler scheduler() throws Exception {
+        return schedulerFactoryBean().getScheduler();
+    }
+
+    /**
+     * Scheduler工厂类
+     * @return
+     * @throws IOException
+     */
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() throws IOException {
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setSchedulerName("Cluster_Scheduler");
+        factory.setDataSource(dataSource);
+        factory.setApplicationContextSchedulerContextKey("applicationContext");
+        factory.setQuartzProperties(quartzProperties());
+        factory.setTaskExecutor(schedulerThreadPool());
+        factory.setStartupDelay(5);//延迟5s执行
+        return factory;
+    }
+
+    /**
+     * 加载配置属性
+     * @return
+     * @throws IOException
+     */
+    @Bean
+    public Properties quartzProperties() throws IOException {
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        propertiesFactoryBean.setLocation(new ClassPathResource("/spring-quartz.properties"));
+        propertiesFactoryBean.afterPropertiesSet();
+        return propertiesFactoryBean.getObject();
+    }
+
+    /**
+     * schedule配置线程池
+     * @return
+     */
+    @Bean
+    public Executor schedulerThreadPool() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors());
+        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors());
+        executor.setQueueCapacity(Runtime.getRuntime().availableProcessors());
+        return executor;
+    }
+
+}
