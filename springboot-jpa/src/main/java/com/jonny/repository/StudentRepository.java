29090b5388c656e577004de9da8974b8c8fe3707
Jonny <e-lijing6@geely.com>
2021-07-22 17:39:59 +0800

    update jpa

--- springboot-jpa/src/main/java/com/jonny/repository/StudentRepository.java
+++ springboot-jpa/src/main/java/com/jonny/repository/StudentRepository.java
@@ -2,8 +2,9 @@ package com.jonny.repository;
 
 import com.jonny.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long>, JpaSpecificationExecutor<Student> {
 
 
 }
