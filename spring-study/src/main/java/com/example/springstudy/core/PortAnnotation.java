298af55e433ea96cc2e0154b16febd94ceacea85
Jonny <342418262@qq.com>
2022-03-27 16:15:40 +0800

    udpate

---
+++ spring-study/src/main/java/com/example/springstudy/core/PortAnnotation.java
@@ -0,0 +1,15 @@
+package com.example.springstudy.core;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Value("${server.port}")
+public @interface PortAnnotation {
+
+}
