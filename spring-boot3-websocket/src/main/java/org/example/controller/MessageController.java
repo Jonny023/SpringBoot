89966e7e7690a44a155417155857396ec2173739
Jonny <342418262@qq.com>
2024-09-19 15:28:47 +0800

    websocket wss

--- spring-boot3-websocket/src/main/java/org/example/controller/MessageController.java
+++ spring-boot3-websocket/src/main/java/org/example/controller/MessageController.java
@@ -3,6 +3,7 @@ package org.example.controller;
 import org.example.utils.RedisUtil;
 import org.example.websocket.server.WebSocketServer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,13 @@ public class MessageController {
     @Autowired
     private RedisUtil redisUtil;
 
+    @Autowired
+    private Environment environment;
+
     @PostMapping("/msg/send")
     public String sendMsg(@RequestParam("msg") String msg, @RequestParam("username") String username) {
         // 推送站内信webSocket
         redisUtil.publish(WebSocketServer.TOPIC_PREFIX + username, msg);
-        return "ok" + System.getProperty("server.port");
+        return "ok" + environment.getProperty("server.port");
     }
 }
